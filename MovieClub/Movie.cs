9b78d6eebaad51e82cd6671e4bf03d0d6e24a6d8
Tomáš David <115162566+Tomas-David@users.noreply.github.com>
2026-02-19 09:59:01 +0100

    zadani

---
+++ MovieClub/Movie.cs
@@ -0,0 +1,14 @@
+﻿namespace MovieClub
+{
+    public class Movie
+    {
+        public int MovieId { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public string Genre { get; set; }
+        public string AddedBy { get; set; }
+        public int Votes { get; set; }
+        public bool IsWatched { get; set; }
+        public DateTime AddedAt { get; set; }
+    }
+}
